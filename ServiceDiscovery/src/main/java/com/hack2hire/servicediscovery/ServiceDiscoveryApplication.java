185d791db37081bbe2b880460f21b2b6e899a71f
sujanab9 <sujanab9@gmail.com>
2023-05-27 21:48:37 -0400

    updated code

--- ServiceDiscovery/src/main/java/com/hack2hire/servicediscovery/ServiceDiscoveryApplication.java
+++ ServiceDiscovery/src/main/java/com/hack2hire/servicediscovery/ServiceDiscoveryApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class ServiceDiscoveryApplication {
-
+//updated main method
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceDiscoveryApplication.class, args);
 	}
